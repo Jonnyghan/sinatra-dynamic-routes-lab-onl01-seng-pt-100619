31574090410ec36eac2aa082734b5ea75d1288cd
Jonathan Ghandforoush <jonnyghan@gmail.com>
2020-02-14 21:03:06 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,4 +3,18 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
 
+  get "/reversename/:name" do 
+    @username = params[:name]
+    "#{@username.reverse}"
+  end
+  
+  get "/square/:number" do 
+    @num = params[:number]
+    "#{@num.to_i * @num.to_i}"
+  end
+  
+  get "/say/:number/:phrase" do 
+    @num = params[:number]
+    @phrase = params[:phrase]
+    ""
 end
